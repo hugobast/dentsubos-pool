a95258c05362a5fd5145cc177d287e0d27bff5a5
Hugo Bastien <hugobast@gmail.com>
2012-08-23 13:16:18 -0400

    WebSocket / TCPSocket

--- Rakefile
+++ Rakefile
@@ -2,9 +2,7 @@ require_relative 'lib/pool'
 
 task :cache_weather do
   weather = Weather.new
-  pool = PoolTemperature.new
   data = {
-    pool: pool.read, 
     condition: weather.condition, 
     outside: weather.temperature
   }
--- pool.rb
+++ pool.rb
@@ -1,10 +1,47 @@
-require 'sinatra'
+require 'eventmachine'
+require 'em-websocket'
+require 'sinatra/base'
 require_relative 'lib/pool'
 
-get '/now' do
-  temperature = TemperatureStore.latest 
-  @pool = temperature[:pool]
-  @outside = temperature[:outside]
-  @condition = temperature[:condition]
-  erb :index
+EM.run do
+  class Pool < Sinatra::Base
+    get '/now' do
+      temperature = TemperatureStore.latest 
+      @pool = $pool
+      @outside = temperature[:outside]
+      @condition = temperature[:condition]
+      erb :index
+    end
+  end
+
+  @channel = EM::Channel.new
+
+  EM::WebSocket.start host: '0.0.0.0', port: 8806 do |socket|
+    socket.onopen do
+      sid = @channel.subscribe { |data| socket.send data }
+      puts "connected!"
+
+      socket.onclose do
+        @channel.unsubscribe sid
+      end
+    end
+  end
+
+  module TemperatureReceiver
+    attr_accessor :channel
+
+    def receive_data data
+      channel.push data
+      $pool = JSON.parse(data)["temperature"]
+    end
+  end
+
+  EM.start_server "0.0.0.0", 8805, TemperatureReceiver do |connection|
+    connection.channel = @channel
+  end
+
+  Pool.run!
 end
+
+
+
